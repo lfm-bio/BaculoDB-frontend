3bfd13e2f8179ee0ebb4e3fc554b3b74be4e8974
LFM <144485318+lfm-bio@users.noreply.github.com>
2023-12-04 10:54:51 -0300

    update css

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,22 +5,30 @@ function Header(){
 
 
 
-    return (<div className={styles.header}>
-    <h1 className={styles.logo}>BaculoDB</h1>
-
-    <ul className={styles.buttons}>
-        <li><a href="#">Genomes</a></li>
-        <li><a href="#">Proteins</a></li>
-        <li><a href="#">NcRNA</a></li>
-        <li><a href="#">Regulatory Elements</a></li>
-        <li><a href="#">Batch Download</a></li>
-    </ul>
-
-    <form>
-        <input type="text" placeholder="GenBank ID or BaculoDB ID" /> 
-        <button>Search</button>
-    </form>
-    </div>)
+    return (
+        <>
+            <div className={styles.header}> 
+            <h1 className={styles.logo}>BaculoDB</h1>
+            <ul className={styles.buttons}>
+                <li><a href="#">Genomes</a></li>
+                <li><a href="#">Proteins</a></li>
+                <li><a href="#">NcRNA</a></li>
+                <li><a href="#">Regulatory Elements</a></li>
+                <li><a href="#">Batch Download</a></li>
+            </ul>
+            </div>
+        
+            <form className={styles.searchBar}>
+                <select name="db" id="asd">
+                    <option value="db">BaculoDB ID</option>
+                    <option value="db">GenkBank ID</option>
+                    <option value="db">Species Name</option>
+                    <option value="db">Protein Name</option>
+                </select>
+                <input type="text" autofocus="autofocus" /> 
+                <button>Search</button>
+            </form>
+        </>)
 }
 
 
