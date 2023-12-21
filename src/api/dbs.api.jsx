2ffc9575db48d58c880ce1762ca269ce5ee173cb
LFM <144485318+lfm-bio@users.noreply.github.com>
2023-12-21 10:34:50 -0300

    api

--- src/api/dbs.api.jsx
+++ src/api/dbs.api.jsx
@@ -1,10 +1,24 @@
 import axios from "axios";
 
+const defaultOptions = {
+  baseURL: "http://localhost:8000",
+};
+
+const apiInstance = axios.create(defaultOptions);
+
 export const searchByIDName = (searchQuery) => {
-  let genome = `http://localhost:8000/genome?search=${searchQuery}`;
-  let protein = `http://localhost:8000/protein?search=${searchQuery}`;
-  const promGenome = axios.get(genome);
-  const promProtein = axios.get(protein);
+  const promGenome = apiInstance.get("genome", {
+    params: {
+      search: searchQuery,
+    },
+  });
+
+  const promProtein = apiInstance.get("protein", {
+    params: {
+      search: searchQuery,
+    },
+  });
+
   const searchData = Promise.all([promGenome, promProtein]);
 
   return searchData;
