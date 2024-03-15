7f1a1302238a77c4cb41e9e7ca1acc91d15c7265
LFM <144485318+lfm-bio@users.noreply.github.com>
2024-03-15 12:58:53 -0300

    download menu working

--- src/sections/BatchDownload.jsx
+++ src/sections/BatchDownload.jsx
@@ -9,6 +9,95 @@ function DownloadByGenusMorph() {
     type: "genomes",
     genusMorph: "baculoviridae",
   });
+  const [DB, setDB] = useState();
+  const [chosenSeqs, setChosenSeqs] = useState();
+  const [genomeData, setGenomeData] = useState();
+  const [download, setDownload] = useState(false);
+
+  useEffect(() => {
+    mainSearch(" ").then(setDB);
+  }, []);
+
+  useEffect(() => {
+    const genomeInfo = {};
+    if (DB !== undefined) {
+      for (const seq of DB) {
+        if (seq.entry_type === "Genome") {
+          genomeInfo[seq.id] = {
+            genus: seq.genus,
+            reference: seq.reference,
+            // falta ictv en DB
+          };
+        }
+      }
+    }
+    setGenomeData(genomeInfo);
+  }, [DB]);
+
+  useEffect(() => {
+    const filterByType = [];
+    const filterByGenusMorph = [];
+    const filterByIctvRef = [];
+
+    if (DB !== undefined && genomeData !== undefined) {
+      setDownload(false);
+      for (const seq of DB) {
+        switch (downloadOptions.type) {
+          case "genomes":
+            if (seq.entry_type === "Genome") {
+              filterByType.push(seq);
+            }
+            break;
+          case "proteomes":
+          case "orfeomes":
+            if (seq.entry_type === "Protein") {
+              filterByType.push(seq);
+            }
+            break;
+        }
+      }
+
+      for (const seq of filterByType) {
+        if (
+          (seq.entry_type === "Genome" &&
+            genomeData[seq.id].genus === downloadOptions.genusMorph) ||
+          (seq.entry_type === "Protein" &&
+            genomeData[seq.genome_id].genus === downloadOptions.genusMorph) ||
+          downloadOptions.genusMorph === "baculoviridae"
+        ) {
+          filterByGenusMorph.push(seq);
+        }
+      }
+
+      for (const seq of filterByGenusMorph) {
+        if (downloadOptions.ictvReference === "all") {
+          filterByIctvRef.push(seq);
+        } else if (downloadOptions.ictvReference === "wreference") {
+          if (
+            (seq.entry_type === "Genome" && genomeData[seq.id].reference) ||
+            (seq.entry_type === "Protein" &&
+              genomeData[seq.genome_id].reference)
+          ) {
+            filterByIctvRef.push(seq);
+          }
+        }
+      }
+      setChosenSeqs(filterByIctvRef);
+      console.log(filterByIctvRef);
+    }
+  }, [genomeData, downloadOptions]);
+
+  useEffect(() => {
+    if (chosenSeqs !== undefined && download) {
+      makeFasta(chosenSeqs);
+    }
+  }, [download]);
+
+  const handleSubmit = (e) => {
+    console.log("submit");
+    e.preventDefault();
+    setDownload(true);
+  };
 
   const handleChange = (e) => {
     const selectName = e.target.name;
@@ -21,7 +110,7 @@ function DownloadByGenusMorph() {
   return (
     <div className={styles.batchDownload}>
       <h3>Download by genus/morphology</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Download</label>
         <select
           onChange={handleChange}
@@ -54,10 +143,10 @@ function DownloadByGenusMorph() {
           <option value="B">B / GV</option>
           <option value="G">G</option>
           <option value="D">D</option>
-          <option value="snpv">SNPV</option>
-          <option value="mnpv">MNPV</option>
+          {/* <option value="snpv">SNPV</option>
+          <option value="mnpv">MNPV</option> */}
         </select>
-        <button>Download</button>
+        <button type="submit">Download</button>
       </form>
     </div>
   );
